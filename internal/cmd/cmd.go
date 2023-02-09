4a760ca97d9f58f891693cb6918e9148fc05915f
bancangbaize <bancangbaize@gmail.com>
2023-02-09 14:58:10 +0800

    implement login and add response middleware

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
 	"goFrameMall/internal/controller"
+	"goFrameMall/internal/service"
 )
 
 var (
@@ -16,12 +17,17 @@ var (
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 			s := g.Server()
 			s.Group("/", func(group *ghttp.RouterGroup) {
-				group.Middleware(ghttp.MiddlewareHandlerResponse)
+				//group.Middleware(ghttp.MiddlewareHandlerResponse)
+				group.Middleware(
+					service.Middleware().Ctx,
+					service.Middleware().ResponseHandler,
+				)
 				group.Bind(
 					controller.Hello,
 					controller.Rotation, // 轮播图
 					controller.Position, // 手工位
 					controller.Admin,    //管理员信息
+					controller.Login,    //登录
 				)
 			})
 			s.Run()
--- internal/logic/logic.go
+++ internal/logic/logic.go
@@ -6,6 +6,10 @@ package logic
 
 import (
 	_ "goFrameMall/internal/logic/admin"
+	_ "goFrameMall/internal/logic/bizctx"
+	_ "goFrameMall/internal/logic/login"
+	_ "goFrameMall/internal/logic/middleware"
 	_ "goFrameMall/internal/logic/position"
 	_ "goFrameMall/internal/logic/rotation"
+	_ "goFrameMall/internal/logic/session"
 )
