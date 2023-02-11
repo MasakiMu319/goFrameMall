2a76ca1a8d53b60bcf7d63edfef57691b33b1260
bancangbaize <bancangbaize@gmail.com>
2023-02-12 00:42:16 +0800

    implement role operations: update, delete and list

--- api/backend/role.go
+++ api/backend/role.go
@@ -11,3 +11,33 @@ type RoleReq struct {
 type RoleRes struct {
 	RoleId int `json:"role_id"`
 }
+
+type RoleUpdateReq struct {
+	g.Meta `path:"/backend/role/update" tags:"role" method:"post" summary:"修改角色"`
+	Id     uint   `json:"id" v:"required#角色名不能为空" dc:"角色 id"`
+	Name   string `json:"name" v:"required#角色名不能为空" dc:"角色名称"`
+	Desc   string `json:"desc"  dc:"角色描述"`
+}
+
+type RoleUpdateRes struct {
+	RoleId uint `json:"role_id"`
+}
+
+type RoleDeleteReq struct {
+	g.Meta `path:"/backend/role/delete" method:"delete" tags:"管理员" summary:"删除角色"`
+	Id     uint `v:"min:1#请选择需要删除的角色" dc:"角色 id"`
+}
+type RoleDeleteRes struct{}
+
+type RoleGetListCommonReq struct {
+	g.Meta `path:"/backend/role/list" method:"get" tags:"角色列表" summary:"获取角色列表接口"`
+	CommonPaginationReq
+}
+type RoleGetListCommonRes struct {
+	//g.Meta `mime:"text/html" type:"string" example:"<html/>"`
+	// TODO: 前后端分离项目不能直接返回 HTML
+	List  interface{} `json:"list" description:"列表"`
+	Page  int         `json:"page" description:"分页码"`
+	Size  int         `json:"size" description:"分页数量"`
+	Total int         `json:"total" description:"数据总数"`
+}
